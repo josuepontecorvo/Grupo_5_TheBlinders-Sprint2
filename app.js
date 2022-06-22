bba7e9d94f7703f461c7a14742c0b37ff8e43db3
josuepontecorvo <josuepontecorvo@gmail.com>
2022-06-22 12:20:26 -0300

    Creacion estructura de archivo

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path =  require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname,"./views/index.html")))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
