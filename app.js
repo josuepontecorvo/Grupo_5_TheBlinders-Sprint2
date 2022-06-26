9e40d45334319a060dd769da11b56b21a1155d15
josuepontecorvo <josuepontecorvo@gmail.com>
2022-06-26 12:32:53 -0300

    Carrito: tabla colapsable en resoluciones pequeñas- sin color ni fuente

--- app.js
+++ app.js
@@ -7,4 +7,5 @@ app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname,"./views/index.html")));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
+app.get("/carrito", (req,res) => res.sendFile(path.resolve(__dirname,"views/product-cart.html")));
 app.listen(port, () => console.log(`Listening on port ${port}!`));
