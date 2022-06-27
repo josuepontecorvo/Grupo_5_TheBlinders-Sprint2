de93e689d9d416dbbeb7c29268c85ae71a2b35a5
josuepontecorvo <josuepontecorvo@gmail.com>
2022-06-26 23:35:59 -0300

    Estructura detalle de producto sin fondo, fuente, header ni footer.

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const port = 3000;
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname,"./views/index.html")));
-app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
-app.get("/carrito", (req,res) => res.sendFile(path.resolve(__dirname,"views/product-cart.html")));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname,'./views/index.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname,'./views/register.html')));
+app.get('/carrito', (req,res) => res.sendFile(path.resolve(__dirname,'views/product-cart.html')));
+app.get('/detalle', (req,res) => res.sendFile(path.resolve(__dirname,'views/productDetail.html')));
+
 app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+
